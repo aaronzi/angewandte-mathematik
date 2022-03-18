8de0c2da9f1258a6154af4a12fa8ebd3d12a4ebf
aaron_zi <aaron.zi@web.de>
2022-03-18 09:37:33 +0100

    Kapitel zur linearen stationären Gleichung abgeschlossen

--- Matlab_Routinen/2_1_linear/exper_stationaer_lin.m
+++ Matlab_Routinen/2_1_linear/exper_stationaer_lin.m
@@ -1,3 +1,4 @@
+%% Experimentelle Bestimmung von N
 % importieren der Konstanten
 c = konstanten();
 N =2.^(1:16);
@@ -8,7 +9,7 @@ s = @(z) (S0 * exp(-c.alpha*z));
 maxerr = NaN(size(N));
 for i=1:length(N)-1
     
-    % Näherungsweise Lösung
+    % Nï¿½herungsweise Lï¿½sung
     [z1,u1] = stationaer_lin(s, N(i));
     [z2,u2] = stationaer_lin(s, N(i+1));
     u2s = u2(1:2:end);
@@ -19,19 +20,31 @@ for i=1:length(N)-1
         break
     end
 end
-subplot(2,1,1);
+
 loglog(N, maxerr,'*-','Linewidth', 2, 'Markersize', 5, 'color', 'g');
 legendString = sprintf('N =  %.0f',Nresult);
 legend(legendString);
 grid on
 xlabel('Anzahl der Teilintervalle');
 ylabel('max. abs. Fehler [um]');
-title('Experiment: N-Bestimmung');
 
-subplot(2,1,2);
-plot(z2,u2,'o-','Linewidth', 1, 'Markersize', 3)
-legend('FD Lösung')
+%% Berechnung der Fï¿½lle fï¿½r S_0 = 10^2, 10^3 und 10^4
+
+N_optimal = 512;
+S_0_1 = 10^2;
+s1 = @(z) (S_0_1 * exp(-c.alpha*z));
+S_0_2 = 10^3;
+s2 = @(z) (S_0_2 * exp(-c.alpha*z));
+S_0_3 = 10^4;
+s3 = @(z) (S_0_3 * exp(-c.alpha*z));
+
+[z_lsg_1,u_lsg_1] = stationaer_lin(s1, N_optimal);
+[z_lsg_2,u_lsg_2] = stationaer_lin(s2, N_optimal);
+[z_lsg_3,u_lsg_3] = stationaer_lin(s3, N_optimal);
+
+plot(z_lsg_1,u_lsg_1,'-o',z_lsg_2,u_lsg_2,'-o',z_lsg_3,u_lsg_3,'-o','Linewidth',1,'Markersize', 3)
+legend('S_0 = 10^2', 'S_0 = 10^2', 'S_0 = 10^2')
 grid on
 xlabel('z [um]');
 ylabel('u [1]');
-title('Ladungsträgerdichte');
+title('Lï¿½sungen von s(z) bei verschiedenen S_0');
